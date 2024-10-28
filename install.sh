32e470b3d0fabccd41de93c7925e953d1f03803e
Radosław Szamszur <github@rsd.sh>
2024-10-28 21:41:27 +0000

    fix: removes bashrc and profile prior hm activation

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if ! command -v nix &> /dev/null; then
   . $HOME/.nix-profile/etc/profile.d/nix.sh
 fi
 
+rm -f $HOME/.bashrc $HOME/.bash_profile
 nix build --no-link github:rszamszur/nixos-config#homeConfigurations.coder.activationPackage
 if [[ ! $(which home-manager) ]]; then
   "$(nix path-info github:rszamszur/nixos-config#homeConfigurations.coder.activationPackage)"/activate
