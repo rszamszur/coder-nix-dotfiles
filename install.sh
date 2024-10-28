a6ab83d50eb39285d6dba809ee1fa07754f167ee
Radosław Szamszur <github@rsd.sh>
2024-10-28 21:35:51 +0000

    fix: loads nix if not present in PATH

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
+if ! command -v nix &> /dev/null; then
+  . $HOME/.nix-profile/etc/profile.d/nix.sh
+fi
+
 nix build --no-link github:rszamszur/nixos-config#homeConfigurations.coder.activationPackage
 if [[ ! $(which home-manager) ]]; then
   "$(nix path-info github:rszamszur/nixos-config#homeConfigurations.coder.activationPackage)"/activate
